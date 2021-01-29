bb6de9070734be047d145101cd9cf65228b08427
Alex <alejandroripio@gmail.com>
2021-01-29 10:41:54 +0100

    DIALOGO MODAL

--- app/src/main/java/com/siglas/interfaces/MainActivity.java
+++ app/src/main/java/com/siglas/interfaces/MainActivity.java
@@ -1,7 +1,10 @@
 package com.siglas.interfaces;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -24,5 +27,56 @@ public class MainActivity extends AppCompatActivity {
                         "Collapsed!", Toast.LENGTH_SHORT).show();
             }
         });
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+
+        showAlertDialogButtonClicked(MainActivity.this);
+
+
+
     }
+    // *** DIALOGO MODAL ***
+    public void showAlertDialogButtonClicked(MainActivity view) {
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //el dialogo estandar tiene título/icono pero podemos sustituirlo
+        //por un XML a medida
+        // builder.setTitle("Achtung!");
+        // builder.setMessage("Where do you go?");
+        // builder.setIcon(R.drawable.ic_action_name_dark);
+        // un XML a medida para el diálogo
+
+        builder.setView(getLayoutInflater().inflate(R.layout.alertdialog_view,
+                null));
+        // add the buttons
+        builder.setPositiveButton("Glide", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something like...
+                        dialog.dismiss();
+                    }
+                });
+        builder.setNegativeButton("ChatBot", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something like...
+                        dialog.dismiss();
+                    }
+                });
+        builder.setNeutralButton("MotionLayout", new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something like...
+                        dialog.dismiss();
+                    }
+                });
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
 }
